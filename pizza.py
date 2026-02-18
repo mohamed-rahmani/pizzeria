8843eccf94eca4810702bb81969833218d71bc4e
mohamed-rahmani <mohamed.rahmani.ma@gmail.com>
2026-02-18 14:12:24 +0100

    fix: oublie d'initialiser l'attribut ingredients par une liste vide

--- pizza.py
+++ pizza.py
@@ -2,5 +2,6 @@ class Pizza:
     def __init__(self, nom: str, ingredients: list[str], prix: float):
         self.nom = nom
         self.prix = prix
+        self.ingredients = []
         for item in ingredients:
             self.ingredients.append(item)
