a6fa034925c70781fa50b3b72ef3e846e38d8c15
mohamed-rahmani <mohamed.rahmani.ma@gmail.com>
2026-02-18 13:47:34 +0100

    feat: classe pizza implemente

---
+++ pizza.py
@@ -0,0 +1,6 @@
+class Pizza:
+    def __init__(self, nom: str, ingredients: list[str], prix: float):
+        self.nom = nom
+        self.prix = prix
+        for item in ingredients:
+            self.ingredients.append(item)
